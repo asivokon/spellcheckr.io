98851d07c3cd5bf9574a14a8cc4c97a92111eb3a
Sergey Podgurskiy <spodgurskiy@gmail.com>
2014-12-06 18:48:13 +0200

    Added FireBaseStore

---
+++ frontend/src/js/stores/FireBaseStore.js
@@ -0,0 +1,42 @@
+var Dispatcher = require('../dispatcher/Dispatcher');
+var Constants = require('../constants/Constants');
+var EventEmitter = require('events').EventEmitter;
+var FireBaseUtil = require('../utils/FireBaseUtils');
+var assign = require('object-assign');
+
+var AT = Constants.ActionTypes,
+    CHANGE_EVENT = Constants.Events.CHANGE,
+    snippetId = null,
+    FireBaseStore = assign({}, EventEmitter.prototype, {
+
+        emitChange: function () {
+            this.emit(CHANGE_EVENT);
+        },
+
+        addChangeListener: function (callback) {
+            this.on(CHANGE_EVENT, callback);
+        },
+
+        removeChangeListener: function (callback) {
+            this.removeListener(CHANGE_EVENT, callback);
+        }
+    });
+
+FireBaseStore.dispatchToken = Dispatcher.register(function (payload) {
+    var action = payload.action;
+    switch (action.type) {
+        case AT.SET_SNIPPET_ID:
+            snippetId = action.snippetId;
+            break;
+        case AT.UPDATE_TEXT:
+
+            break;
+        default:
+        // do nothing
+    }
+
+});
+
+module.exports = FireBaseStore;
+
+
