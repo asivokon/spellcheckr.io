8d31ae47989bee98bd420afcee372006c972b187
Sergey Podgurskiy <spodgurskiy@gmail.com>
2014-12-06 15:04:52 +0200

    Fixed pubnub

--- frontend/src/js/utils/PubnubUtils.js
+++ frontend/src/js/utils/PubnubUtils.js
@@ -4,7 +4,7 @@ var Pubnub = require('pubnub-browserify');
 module.exports = {
 
     init: function () {
-        Pubnub.init({
+        Pubnub = Pubnub.init({
             publish_key: 'pub-c-9deee8d8-bc12-4770-9075-f125b8de84dd',
             subscribe_key: 'sub-c-56f28a4a-7d24-11e4-baaa-02ee2ddab7fe'
         });
@@ -14,14 +14,16 @@ module.exports = {
     publish: function (text) {
         Pubnub.publish({
             channel: 'test-typing',
-            message: {"typing": text}
+            message: {"typing": text},
+            callback  : function(e) { console.log( "SUCCESS!", e ); },
+            error     : function(e) { console.log( "FAILED! RETRY PUBLISH!", e ); }
         });
     },
 
     subscribe: function () {
         Pubnub.subscribe({
             channel: 'test-typing',
-            message: function (m) {
+            callback: function (m) {
                 console.log('got', m);
                 ApiActions.pubnubUpdate(m.typing);
             }
