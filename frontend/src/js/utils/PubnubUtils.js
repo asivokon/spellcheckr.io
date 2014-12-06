168475b4785fe7fc4d7b98753da2972c9b0d4caf
Sergey Podgurskiy <spodgurskiy@gmail.com>
2014-12-06 14:59:23 +0200

    Added pubnub-browserify

--- frontend/src/js/utils/PubnubUtils.js
+++ frontend/src/js/utils/PubnubUtils.js
@@ -1,10 +1,10 @@
 var ApiActions = require('../actions/ApiActions');
-var Pubnub;
+var Pubnub = require('pubnub-browserify');
 
 module.exports = {
 
     init: function () {
-        Pubnub = PUBNUB.init({
+        Pubnub.init({
             publish_key: 'pub-c-9deee8d8-bc12-4770-9075-f125b8de84dd',
             subscribe_key: 'sub-c-56f28a4a-7d24-11e4-baaa-02ee2ddab7fe'
         });
