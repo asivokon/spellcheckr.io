493d2f6e291c45850f46fe40cd822c3c3c8d4fc7
Oleksiy Syvokon <oleksiy.syvokon@gmail.com>
2014-12-08 05:49:12 +0200

    No, really

--- frontend/src/js/components/Workspace.react.js
+++ frontend/src/js/components/Workspace.react.js
@@ -47,7 +47,6 @@ module.exports = React.createClass({
                     <div className="workspace-container">
                         <div className="column">
                             <TextEditView state={appState} />
-                            // <LanguageSelect selectedLanguage='eng'/>
                         </div>
                         <div className="column">
                             <AnswersView state={appState} />
